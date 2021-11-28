081bc64b3fe7568a9b47131f0e72db60f6168c0f
jiangshan00001 <710806594@qq.com>
2021-11-28 21:07:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 	
 setuptools.setup(
     name="django_amis_render",
-    version="0.0.5",
+    version="0.0.6",
     author="jiangshan00000",
     author_email="710806594@qq.com",
     description="a django app to render amis json file to web.",
